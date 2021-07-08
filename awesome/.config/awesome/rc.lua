9580d4142391ad2dced5c793b325db1082cc61e6
chillmelon <chiyuh1996@gmail.com>
2021-07-08 18:21:37 +0800

    added volume control widget

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -9,12 +9,14 @@ local awful = require("awful")
 require("awful.autofocus")
 -- Widget and layout library
 local wibox = require("wibox")
+local APW = require("apw/widget")
 -- Theme handling library
 local beautiful = require("beautiful")
 -- Notification library
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
+
 
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
@@ -216,6 +218,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
+            APW,
             mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
