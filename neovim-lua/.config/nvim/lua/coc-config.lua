f7c9b9c4aecbc368b02c540add63ee4a41e55563
chillmelon <chiyuh1996@gmail.com>
2022-07-04 23:08:11 +0800

    update neovim

--- neovim-lua/.config/nvim/lua/coc-config.lua
+++ neovim-lua/.config/nvim/lua/coc-config.lua
@@ -2,6 +2,8 @@ vim.api.nvim_set_keymap("i", "<CR>", 'pumvisible() ? coc#_select_confirm() : "<C
 vim.api.nvim_set_keymap("n", "<leader>.", "<Plug>(coc-codeaction)", {})
 vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
 vim.api.nvim_set_keymap("n", "gr", "<Plug>(coc-references)", {silent = true})
+vim.api.nvim_set_keymap("n", "<leader>qf", "<Plug>(coc-fix-current)", {silent = true})
+vim.api.nvim_set_keymap("n", "<leader>qq", "<Plug>(coc-codeaction-cursor)", {silent = true})
 vim.api.nvim_set_keymap("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
 vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
 vim.o.hidden = true
