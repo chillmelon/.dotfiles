72dfc4301cbe3c8ea9708dce422efdf0a9138017
Jason Hsieh <Jason.CY.Hsieh@ibm.com>
2023-11-23 19:13:28 +0800

    add: lsp keymap

--- minimal-neovim/.config/nvim/lua/plugins/lsp.lua
+++ minimal-neovim/.config/nvim/lua/plugins/lsp.lua
@@ -64,12 +64,30 @@ return {
     config = function()
       -- This is where all the LSP shenanigans will live
       local lsp_zero = require('lsp-zero')
+      local function quickfix()
+        vim.lsp.buf.code_action({
+          filter = function(a) return a.isPreferred end,
+          apply = true
+        })
+      end
       lsp_zero.extend_lspconfig()
 
       lsp_zero.on_attach(function(client, bufnr)
         -- see :help lsp-zero-keybindings
         -- to learn the available actions
         lsp_zero.default_keymaps({ buffer = bufnr })
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+        vim.keymap.set('n', '<leader>ql', vim.diagnostic.setloclist)
+        vim.keymap.set('n', '<leader>qf', quickfix, {})
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+        vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+        vim.keymap.set('n', '<leader>f', function()
+          vim.lsp.buf.format { async = true }
+        end, {})
       end)
 
       require('mason-lspconfig').setup({
